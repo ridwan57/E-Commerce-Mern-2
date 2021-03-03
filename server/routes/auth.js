f0bb1eccce3e6a552caf3c17f72adb182c20b001
ridwan57 <ridwananik57@gmail.com>
2021-03-04 03:22:10 +0600

    lesson40

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createOrUpdateUser } = require('../controllers/auth')
+const { createOrUpdateUser, currentUser } = require('../controllers/auth')
 const router = express.Router()
 // middlewares
 
@@ -7,16 +7,6 @@ const { authCheck } = require('../middlewares/auth');
 
 // controllers
 router.post('/create-or-update-user', authCheck, createOrUpdateUser)
+router.post('/current-user', authCheck, currentUser)
 
-const myMiddleware = (req, res, next) => {
-    console.log('I am Midlle')
-    next()
-
-}
-
-router.get('/testing', myMiddleware, (req, res) => {
-    res.json({
-        data: 'testing'
-    })
-})
 module.exports = router 
